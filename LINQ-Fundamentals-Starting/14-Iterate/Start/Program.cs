1b479804d61017b5d36722081390b715b33af33b
binhk <binhkhuu90@gmail.com>
2023-11-18 17:40:48 +0800

    iterate over collection with linq

--- LINQ-Fundamentals-Starting/14-Iterate/Start/Program.cs
+++ LINQ-Fundamentals-Starting/14-Iterate/Start/Program.cs
@@ -4,7 +4,12 @@
 SamplesViewModel vm = new();
 
 // Call Sample Method
-var result = vm.ForEachQuery();
+//var result = vm.ForEachQuery();
+//var result = vm.ForEachMethod();
+//var result = vm.ForEachSubQueryMethod();
+//var result = vm.ForEachSubQueryQuery();
+//var result = vm.ForEachQueryCallingMethodQuery();
+var result = vm.ForEachQueryCallingMethod();
 
 // Display Results
 vm.Display(result);
